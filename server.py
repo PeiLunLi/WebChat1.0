c08c2b6e9a8c6ddf4af2f98a9ce160d1b3f02bdf
peiluner <30365887+PeiLunLi@users.noreply.github.com>
2017-12-04 23:50:55 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ from tornado import ioloop
 import tornado
 from tornado import httpserver
 from application import Application
+
 import config
+
 
 if __name__ == '__main__':
     app = Application()
